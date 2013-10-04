2c3fb46dd68862b54a25efc392c1fc8c0a87002b
Jason Hagglund <jasonh@onthecity.org>
2013-10-04 14:35:07 -0700

    Removing more sensitive stuff...

--- spec/helper.rb
+++ spec/helper.rb
@@ -28,15 +28,12 @@ RSpec.configure do |config|
   end
 end
 
-APP_ID = "aee1e51abe07366e3a11d4dfc0f95e17d58a1e3c45a8d3016c8a427459ba2a3b"
-SECRET = "7cc1c16604a021b43ce5b49a2c2fb808c23b33d998e76ca866475a5a8ca44a04"
-
 def fire_up_test_client(username, password, subdomain)
   ENV['THECITY_SUBDOMAIN'] = subdomain
   client = TheCity::API::Client.new do |config|
-    config.app_id        = APP_ID
-    config.app_secret    = SECRET
-    config.access_token  = get_oauth_token_for(username, password, subdomain)
+    config.app_id        = ''
+    config.app_secret    = ''
+    config.access_token  = ''
   end
   return client
 end
